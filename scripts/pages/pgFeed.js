b8e63957d91784c4bbd08171685732de24e2383a
Alper Ozisik <alper.ozisik@smartface.io>
2017-10-09 15:52:24 +0000

    work on the list

--- scripts/pages/pgFeed.js
+++ scripts/pages/pgFeed.js
@@ -1,6 +1,3 @@
-/* 
-		You can modify its contents.
-*/
 const extend = require('js-base/core/extend');
 const PgFeedDesign = require('ui/ui_pgFeed');
 const ListViewItem = require('sf-core/ui/listviewitem');
@@ -9,11 +6,8 @@ const ListViewLoadItem = require("../components/ListViewLoadItem");
 const PgFeed = extend(PgFeedDesign)(
   // Constructor
   function(_super) {
-    // Initalizes super class for this page scope
     _super(this);
-    // overrides super.onShow method
     this.onShow = onShow.bind(this, this.onShow.bind(this));
-    // overrides super.onLoad method
     this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
 
   });
@@ -26,6 +20,8 @@ const PgFeed = extend(PgFeedDesign)(
  */
 function onShow(superOnShow) {
   superOnShow();
+  const page = this;
+  fetchData.call(page);
 }
 
 /**
@@ -38,6 +34,14 @@ function onLoad(superOnLoad) {
   const page = this;
   const lvFeed = page.lvFeed;
   lvFeed.refreshEnabled = false;
+  // lvFeed.rowHeight = 140;
+
+  lvFeed.onRowHeight = function(index) {
+    var height = index === (page.feedData.length - 1) ?
+      100 : 140;
+    console.log(`row height for index ${index} is ${height}`);
+    return height;
+  };
 
   lvFeed.onRowCreate = function() {
     var listViewItem = new ListViewItem();
@@ -47,24 +51,40 @@ function onLoad(superOnLoad) {
     listViewLoadItem.width = NaN;
     listViewItem.addChild(listViewLoadItem);
     listViewItem.addChild(feedItem);
-
-
+    // console.log("row created");
     return listViewItem;
   };
 
   lvFeed.onRowBind = function(listViewItem, index) {
-    var lastRow = index === (lvFeed.itemCount - 1);
-    var feedItem = listViewItem.findChildById(1110);
-    var listViewLoadItem = listViewItem.findChildById(1010);
-    feedItem.visible = !lastRow;
-    listViewLoadItem.visible = lastRow;
+    try {
+      var lastRow = index === (page.feedData.length - 1);
+      var feedItem = listViewItem.findChildById(1110);
+      var listViewLoadItem = listViewItem.findChildById(1010);
+      feedItem.visible = !lastRow;
+      listViewLoadItem.visible = lastRow;
+    }
+    catch (ex) {}
+    // console.log(`row bind for index ${index}`);
   };
 
-  lvFeed.onRowHeight = (index) => {
-    return index === (lvFeed.itemCount - 1) ?
-      100 : 140;
-  };
 
+}
+
+function fetchData() {
+  const page = this;
+
+  setTimeout(() => {
+    page.feedData = [];
+    page.feedData.length = 50;
+    loadData.call(page);
+  }, 300);
+
+}
+
+function loadData() {
+  const page = this;
+  page.lvFeed.itemCount = page.feedData.length;
+  page.lvFeed.refreshData();
 
 }
 
